dd5395ea847175995f2e3e9e54d10e6454b94923
Kok Sing Khong <khongks@gmail.com>
2022-09-21 22:40:56 +1000

    GitOps customizations for organization and cluster
    Signed-off-by: Kok Sing Khong <khongks@gmail.com>

--- scripts/bootstrap-gitea.sh
+++ scripts/bootstrap-gitea.sh
@@ -15,7 +15,7 @@ popd () {
 
 
 set +e
-oc version --client | grep '4.7\|4.8'
+oc version --client | grep '4.7\|4.8\|4.9|\4.10'
 OC_VERSION_CHECK=$?
 set -e
 if [[ ${OC_VERSION_CHECK} -ne 0 ]]; then
